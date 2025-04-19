2106b443a20a9995b50a98eaa85ca9d10dd92e2b
Benlox <131167469+Benlox44@users.noreply.github.com>
2025-04-19 04:05:07 -0400

    decorator @CurrentUser

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, ParseIntPipe, Body, Post, Patch, Get, Delete, Param } from '@nestjs/common';
-import { UseGuards, Req } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { AuthRequest } from 'src/auth/types/auth-request.interface';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user-dto';
 import { SafeUser } from './types/safe-user-type';
 import { UpdateUserEmailDto } from './dto/update-user-email.dto';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { JwtPayload } from 'src/auth/types/jwt-payload.interface';
 
 @Controller('users')
 export class UsersController {
@@ -24,23 +25,23 @@ export class UsersController {
 
   @UseGuards(AuthGuard('jwt'))
   @Patch('me')
-  async update(@Req() req: AuthRequest, @Body() dto: UpdateUserDto): Promise<SafeUser> {
-    return this.usersService.editProfile(req.user.sub, dto);
+  async update(@CurrentUser() user: JwtPayload, @Body() dto: UpdateUserDto): Promise<SafeUser> {
+    return this.usersService.editProfile(user.sub, dto);
   }
 
   @UseGuards(AuthGuard('jwt'))
   @Patch('me/email')
   async requestEmailChange(
-    @Req() req: AuthRequest,
+    @CurrentUser() user: JwtPayload,
     @Body() dto: UpdateUserEmailDto,
   ): Promise<{ message: string }> {
-    return this.usersService.requestEmailChange(req.user.sub, dto);
+    return this.usersService.requestEmailChange(user.sub, dto);
   }
 
   @UseGuards(AuthGuard('jwt'))
   @Delete('me')
-  async deleteMe(@Req() req: AuthRequest): Promise<{ message: string }> {
-    await this.usersService.delete(req.user.sub);
+  async deleteMe(@CurrentUser() user: JwtPayload): Promise<{ message: string }> {
+    await this.usersService.delete(user.sub);
     return { message: 'Your account was deleted successfully' };
   }
 
